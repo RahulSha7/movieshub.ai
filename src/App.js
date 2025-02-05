0343bdc0b4b6d79bd8b9e0ab12d2627b60d0edf2
RahulSha7 <rahulsharma312004@gmail.com>
2025-02-06 00:16:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-green-700 text-3xl font-bold'> initially beginnig</h1>
+    </div>
+  );
+}
+
+export default App;
